7799cf73598a971fd2fc6fb34769bbd7610fade6
aryankhatana01 <aryankhatana01@gmail.com>
2022-10-25 02:36:02 +0530

    Changed title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button class="twitter-button">Tweet</button>
+      <button class="sub-button">SUBSCRIBE</button>
+      <button class="join-button">JOIN</button>
     </div>
   );
 }
