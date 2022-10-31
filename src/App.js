a71f4ec42f1700f6e51209212e3738711a3685be
aryankhatana01 <aryankhatana01@gmail.com>
2022-10-31 15:33:19 +0530

    Fixed responsiveness

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ export default function App() {
       <img src={icon} alt="unblurai-logo" className='logo'/>
       <Navbar />
       <h1 className='primary-heading'>Unblur your old blurry pictures using our AI Model for free!</h1>
+      <div className='sketch'>
+        <svg width="105" height="40" viewBox="0 0 156 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M7.50002 37.5C-32.5001 13.5 192 -9.50024 148 9.49994" stroke="url(#paint0_linear_2_15)" stroke-width="4"/>
+          <defs>
+          <linearGradient id="paint0_linear_2_15" x1="78.7632" y1="5.25085" x2="78.7632" y2="37.0002" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#21A0B4"/>
+          <stop offset="1" stop-color="#4933C7" stop-opacity="0.62"/>
+          </linearGradient>
+          </defs>
+        </svg>
+      </div>
     </div>
   );
 }
