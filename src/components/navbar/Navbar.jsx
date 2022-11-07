3b76c17f8751d32f4617a17233b5fe469f6dd096
aryankhatana01 <aryankhatana01@gmail.com>
2022-11-08 00:19:04 +0530

    Added hamburger

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-// import React, { useState } from "react";
-// import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import icon from '../../assets/unblurai-text-new.png';
 import './navbar.css';
 
 const Navbar = () => {
-    // const [toggleMenu, setToggleMenu] = useState(false);
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
         <div className='box'>
             <div className="navbar">
@@ -18,6 +20,26 @@ const Navbar = () => {
                 </div>
                 <div button className="navbar__button">
                     <button className="button">Get Started</button>
+                </div>
+                <div className="navbar-hamburger">
+                    {
+                        toggleMenu 
+                        ? <RiCloseLine className="hamburger" size={28} onClick={() => setToggleMenu(false)} /> 
+                        : <RiMenu3Line className="hamburger" size={28} onClick={() => setToggleMenu(true)} />
+                    }
+                    {
+                        toggleMenu && (
+                            <div className="navbar-hamburger-container scale-up-center">
+                                <div className="navbar-hamburger-links">
+                                    <ul className="main-menu-ham">
+                                        <li className="nav-link-ham"><a className="nav-link-buttons-ham" href="/home">Home</a></li>
+                                        <li className="nav-link-ham"><a className="nav-link-buttons-ham" href="/about">About</a></li>
+                                        <li className="nav-link-ham"><a className="nav-link-buttons-ham" href="/contact">Contact</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </div>
